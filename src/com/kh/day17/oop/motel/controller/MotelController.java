61a82e97071c5813bb9cee51a8f1aad328efc000
ssuye <tndp9922@gmail.com>
2023-07-19 16:57:27 +0900

    [FIX] 23/07/19 Motel Controller, View 수정, 1~10번방 외 선택시 예외처리 수정
    1. checkIn, checkOut 시 성공여부를 활용하여 View를 이용한 결과값 출력하도록 수정
    2. 변수 roomState -> changeYn 으로 변경, changeYn은 checkIn()의 성공여부를 리턴 받음
    3. Controller의 checkIn(), checkOut() 메소드 변경 및 checkRoomState() 메소드 삭제
    4. MotelView에서 논리연산자 && -> ||로 변경
    5. MotelRun에서 입실과 퇴실의 경우 리턴값이 -1이 아닐때에만 동작하도록 코드 변경

--- src/com/kh/day17/oop/motel/controller/MotelController.java
+++ src/com/kh/day17/oop/motel/controller/MotelController.java
@@ -19,62 +19,26 @@ public class MotelController {
 		}
 	}
 	
-	public void printAllRooms() {
-		for(int i = 0; i < rList.size(); i++) {
-			if(!rList.get(i)) {
-				System.out.println((i +1) + "번 방이 현재 비어있습니다.");
-			}
-			else {
-				System.out.println((i +1) + "번 방에는 현재 손님이 있습니다.");
-			}
-		}
+	public List<Boolean> getAllRooms() {
+		return rList;
 	}
 	
-	public void checkIn(int roomNo) {
-		if(roomNo > 0 && roomNo <= 10) {
-//			Room room = rList.get(roomNo -1);
-			Boolean room = rList.get(roomNo -1);
-//			if(!room.isAvailable()) {
-			if(!room) {
-//				room.setAvailable(true);
-				rList.set(roomNo -1, true);
-				System.out.println(roomNo + "번 방에 입실하셨습니다." );
-			}
-			else {
-				System.out.println(roomNo + "번 방은 현재 손님이 있습니다.");
-			}
-			try {
-				Thread.sleep(250);
-			} 
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	public Boolean checkIn(int roomNo) {
+		Boolean flag = false;
+		if(!rList.get(roomNo-1)) {
+			rList.set(roomNo-1, true);
+			return !flag;
 		}
-		else {
-			System.out.println("1번 방부터 10번 방까지 준비되어있습니다.");
-		}
+		return flag;
 	}
 	
-	public void checkOut(int chosenRoom) {
-		if(chosenRoom > 0 && chosenRoom <= 10) {
-			Boolean room = rList.get(chosenRoom -1);
-			if(room) {
-				rList.set(chosenRoom -1, false);
-				System.out.println(chosenRoom + "번 방에 퇴실하셨습니다." );
-			}
-			else {
-				System.out.println(chosenRoom + "번 방은 현재 빈 방입니다.");
-			}
-			try {
-				Thread.sleep(250);
-			} 
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	public Boolean checkOut(int chosenRoom) {
+		Boolean flag = false;
+		if(rList.get(chosenRoom-1)) {
+			rList.set(chosenRoom-1, false);
+			return !flag;
 		}
-		else {
-			System.out.println("1번 방부터 10번 방까지 준비되어있습니다.");
-		}
+		return flag;
 	}
 }
 			
--- src/com/kh/day17/oop/motel/view/MotelView.java
+++ src/com/kh/day17/oop/motel/view/MotelView.java
@@ -1,5 +1,6 @@
 package com.kh.day17.oop.motel.view;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class MotelView {
@@ -17,6 +18,38 @@ public class MotelView {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("몇 번 방에 " + category + "하시겠습니까? ");
 		int roomNo = sc.nextInt();
-		return roomNo;
+		if(roomNo < 0 || roomNo > 10) {
+			System.out.println("1번 방부터 10번방까지 준비되어 있습니다.");
+			return -1;
+		}else {
+			return roomNo;
+		}
+	}
+
+	public void roomCheckin(Boolean changeYn, int roomNo) throws Exception {
+		if(changeYn) {
+			System.out.println(roomNo + "번 방에 입실하셨습니다.");
+		}
+		else {
+			System.out.println(roomNo + "번방은 현재 손님이 있습니다.");
+		}
+	}
+
+	public void roomCheckOut(Boolean changeYn, int roomNo) throws Exception {
+		if(changeYn) 
+			System.out.println(roomNo + "번 방에 퇴실하셨습니다.");
+		else 
+			System.out.println(roomNo + "번방은 현재 빈 방입니다.");
+		
+	}
+
+	public void printAllRooms(List<Boolean> rList) {
+		for(int i = 0; i < rList.size(); i++) {
+			if(!rList.get(i))
+				System.out.println((i+1)+"번 방이 현재 비어 있습니다.");
+			else
+				System.out.println((i+1)+"번 방에는 현재 손님이 있습니다.");
+		}
+		
 	}
 }
